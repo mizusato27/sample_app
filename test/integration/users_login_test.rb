39f3c32f7621ce5e422a76c161abda500a909314
佐藤瑞起 <117ema3cia@gmail.com>
2025-08-18 18:13:06 +0900

    9章終了

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,47 +1,5 @@
 require "test_helper"
 
-# class UsersLoginTest < ActionDispatch::IntegrationTest
-
-#   def setup
-#     @user = users(:michael)
-#   end
-
-#   test "login with valid email/invalid password" do
-#     get login_path
-#     assert_template 'sessions/new'
-#     post login_path, params: { session: { email:    @user.email,
-#                                           password: "invalid" } }
-#     assert_not is_logged_in?
-#     assert_response :unprocessable_entity
-#     assert_template 'sessions/new'
-#     assert_not flash.empty?
-#     get root_path
-#     assert flash.empty?
-#   end
-
-#   test "login with valid information followed by logout" do
-#     post login_path, params: { session: { email:    @user.email,
-#                                           password: 'password' } }
-#     assert is_logged_in?
-#     assert_redirected_to @user
-#     follow_redirect!
-#     assert_template 'users/show'
-#     assert_select "a[href=?]", login_path, count: 0
-#     assert_select "a[href=?]", logout_path
-#     assert_select "a[href=?]", user_path(@user)
-#     delete logout_path
-#     assert_not is_logged_in?
-#     assert_response :see_other
-#     assert_redirected_to root_url
-#     follow_redirect!
-#     assert_select "a[href=?]", login_path
-#     assert_select "a[href=?]", logout_path,      count: 0
-#     assert_select "a[href=?]", user_path(@user), count: 0
-#   end
-# end
-
-# 8.3演習2でのコード
-
 class UsersLogin < ActionDispatch::IntegrationTest
 
   def setup
@@ -113,5 +71,26 @@ class LogoutTest < Logout
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
+
+class RememberingTest < UsersLogin
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    # assert_not cookies[:remember_token].blank?
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
